8f9bb3b8b482a77ce87e8c71b0e11aeb8a809634
curlygirly <geo18005@byui.edu>
2023-09-15 13:04:15 -0500

    hw

--- lab01.py
+++ lab01.py
@@ -3,33 +3,43 @@
 # 2. Assignment Name: 
 #    Lab 01: Python Review
 # 3. Assignment Description:
-#    -describe what this program is meant to do-
+#    Write a program in python to display a message on the screen, prompt user for information,
+#    make a decision, store data in a list, and loop.
 # 4. What was the hardest part? Be as specific as possible.
-#    -a paragraph or two about how the assignment went for you-
+#    The most difficult things in this assignment would be storing the data. 
+#    Having to set up an array for the data to be store and then having to
+#    store the users answers themselves was a bit tricky. However, once I figured
+#    it out everything else fell into place.
 # 5. How long did it take for you to complete the assignment?
-#    -total time in hours including reading the assignment and submitting the program-  
+#    The assignment took me roughly 3 hours since I had to do additional reading to understand
+#    storing data into an array. I also watched a couple tutorials on YouTube that demonstrated
+#    how to store what the user response is into the array itself.
 
 import random
 
-# Game introduction.
-
-# Prompt the user for how difficult the game will be. Ask for an integer.
-print('This is the "guess a number" game.')
+# ask user
+print('\nThis is the "guess a number" game.')
 print('You try to guess a random number in the smallest number of attempts.')
+value_max = int(input('\nPick a positive integer: '))
 
-# Generate a random number between 1 and the chosen value.
+# generate random number
 value_random = random.randint(1, value_max)
 
-# Give the user instructions as to what he or she will be doing.
+print(f'Guess a number between 1 and {value_max}')
+list = []
+count = 0
 
-# Initialize the sentinal and the array of guesses.
+while True:
+    guess = int(input('> '))
+    list.append(guess)
+    count += 1
 
-# Play the guessing game.
+    if guess > value_random:
+        print('     Too high!')
+    elif guess < value_random:
+        print('     Too low!')
+    else:
+        print(f'You were able to find the number in {count} guesses.')
+        break
 
-    # Prompt the user for a number.
-
-    # Store the number in an array so it can be displayed later.
-
-    # Make a decision: was the guess too high, too low, or just right.
-
-# Give the user a report: How many guesses and what the guesses where.
+print(f'The numbers you guessed were: {list}')
