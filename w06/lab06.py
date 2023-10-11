cfa018c51f5168db2359b0f92e0edb3d0e7e0991
gcelesty <celestegeorge17@gmail.com>
2023-10-11 00:18:29 -0500

    hw

--- w06/lab06.py
+++ w06/lab06.py
@@ -2,10 +2,12 @@ import json
 
 def read_file(filename):
     try:
+        # open the file from input, read the contents, close the file, return contents in an array
         with open(filename, 'r') as file:
             data = json.load(file)
             return data['array']
     except FileNotFoundError:
+        # print error code and return empty list
         print("File not found. Please check the filename and try again.")
         return []
 
@@ -14,28 +16,35 @@ def advanced_search(word_list, search_word):
     end_page = len(word_list) - 1
 
     while start_page <= end_page:
+        # calculate middle page as int, retrive middle word from int array
         middle_page = (start_page + end_page) // 2
         middle_word = word_list[middle_page]
 
         if middle_word == search_word:
             return True
         elif middle_word < search_word:
+            # narrowing the search to the upper half
             start_page = middle_page + 1
         else:
+            # narrowing the search to the lower half
             end_page = middle_page - 1
 
     return False
 
 def main():
+    # prompt for the json filename
     filename = input("What is the name of the file? ")
     word_list = read_file(filename)
 
+    # if file not found end program
     if not word_list:
         return
 
+    # prompt for word to look for
     search_word = input("What name are we looking for? ")
     found = advanced_search(word_list, search_word)
 
+    # if success print found statement, else show error code for no word in file
     if found:
         print(f"We found {search_word} in {filename}.")
     else:
