3a22da8f83f73d6c90860e7f443ee008310c2e95
gcelesty <celestegeorge17@gmail.com>
2023-10-17 19:43:16 -0500

    hw

--- w06/lab06.py
+++ w06/lab06.py
@@ -1,3 +1,23 @@
+# 1. Name:
+#      Celeste George
+# 2. Assignment Name:
+#      Lab 06: Advanced Search
+# 3. Assignment Description:
+#      Implement the advanced search algorithm described in Lab 05. It will prompt
+#      the user for a filname. We will read the contents of the file into a list.
+#      The program will then prompt the user for a word to search for in the file.
+#      And ultimately telling the user if the word was found.
+# 4. Algorithmic Efficiency
+#      The algorithmic efficiency of binary search is O(log n) where 'n' is the number
+#      of elements in the list being searched. It is especially useful when dealing with
+#      with large datasets or when you need to perfrom repeated searches.
+# 5. What was the hardest part? Be as specific as possible.
+#      Trying to get the program to read the files and to find a way to store it.
+#      After figuring that out I had to figure out a way for the code to search through
+#      the array to find the word the user was looking for.
+# 6. How long did it take for you to complete the assignment?
+#      2.5 hours
+
 import json
 
 def read_file(filename):
@@ -5,7 +25,11 @@ def read_file(filename):
         # open the file from input, read the contents, close the file, return contents in an array
         with open(filename, 'r') as file:
             data = json.load(file)
-            return data['array']
+            if 'array' in data and data['array']:
+                return data['array']
+            else:
+                print("The file you are searching is empty.")
+                return []
     except FileNotFoundError:
         # print error code and return empty list
         print("File not found. Please check the filename and try again.")
