016ce5a40cf97aa0cb880b390d1a3297dab9a8f3
gcelesty <celestegeorge17@gmail.com>
2023-11-17 21:20:56 -0600

    hw

--- w10/lab10.py
+++ w10/lab10.py
@@ -1,3 +1,18 @@
+# 1. Name:
+#      Celeste George
+# 2. Assignment Name:
+#      Lab 10: Numeric Sequence
+# 3. Assignment Description:
+#      Python program which will prompt the user for a positive integer n 
+#      and then display the nth Francois number.
+# 4. What was the hardest part? Be as specific as possible.
+#      The most difficult part would be error handling, espically the assertions and
+#      exceptions. Understanding and implementing 'assert' and 'try/except' effectively
+#      in order to flow with the code and make it clear of bugs, but also readable.
+# 5. How long did it take for you to complete the assignment?
+#      5 hours
+
+
 def francois(num):
     assert isinstance(num, int) and num > 0, "Input must be a positive integer"
     
